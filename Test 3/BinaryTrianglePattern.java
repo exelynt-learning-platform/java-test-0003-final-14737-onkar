81ce5a7ecf685115c3d4bc916226bd5089f05eee
onkarkarande77 <karandeonkar05@gmail.com>
2026-03-14 16:28:11 +0530

    Update BinaryTrianglePattern.java

--- Test 3/BinaryTrianglePattern.java
+++ Test 3/BinaryTrianglePattern.java
@@ -1,20 +1,16 @@
-package com.pattern;
-
-public class BinaryTrianglePattern {
+ public class BinaryTrianglePattern {
     public static void main(String[] args) {
 
         int rows = 6;
 
         for (int i = 1; i <= rows; i++) {
+            int value = (i % 2 == 0) ? 0 : 1;
+
             for (int j = 1; j <= i; j++) {
-
-                if ((i + j) % 2 == 0) {
-                    System.out.print("1 ");
-                } else {
-                    System.out.print("0 ");
-                }
-
+                System.out.print(value + " ");
+                value = (value == 1) ? 0 : 1;
             }
+
             System.out.println();
         }
     }
